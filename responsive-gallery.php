7a7a9ae533f037ecc7971c52c7b791fa93e194bf
Arman hossain <armanh688@gmail.com>
2019-10-20 23:41:00 +0600

    responsive gallery

---
+++ responsive-gallery.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/uikit.min.css">
+  <link rel="shortcut icon" type="image/x-icon" href="img/a.png">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+	<title>responsive gallery</title>
+</head>
+<body>
+	  <!-- start gallery -->
+  <section id="gallery" class="py-5" uk-lightbox>
+    <div class="container">
+      <div class="row text-center mb-4">
+        <div class="col">
+          <h3>Photo Gallery</h3>
+          <p class="lead">Check out our photos</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4 mb-4">
+          <div>
+            <a href="img/slider2.jpg">
+              <img src="img/slider2.jpg" alt="" class="img-fluid">
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4 mb-4">
+          <div>
+            <a href="img/slider3.jpg">
+              <img src="img/slider3.jpg" alt="" class="img-fluid">
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4 mb-4">
+          <div>
+            <a href="img/slider1.jpg">
+              <img src="img/slider1.jpg" alt="" class="img-fluid">
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4 mb-4">
+          <div>
+            <a href="img/slider2.jpg">
+              <img src="img/slider2.jpg" alt="" class="img-fluid">
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4 mb-4">
+          <div>
+            <a href="img/slider5.jpg">
+              <img src="img/slider5.jpg" alt="" class="img-fluid">
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4 mb-4">
+          <div>
+            <a href="img/slider2.jpg">
+              <img src="img/slider2.jpg" alt="" class="img-fluid">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end gallery -->
+   <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/uikit.min.js"></script>
+  <script src="js/uikit-icons.min.js"></script>
+</body>
+</html>
